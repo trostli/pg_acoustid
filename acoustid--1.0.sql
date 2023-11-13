aab644fcbf9d553a27cc1635d091245beb43aaf4
trostli <daniel.trostli@gmail.com>
2023-11-13 15:38:38 -0500

    use bigint/int8 instead for compatability with storing chromaprints default unsigned ints

--- acoustid--1.0.sql
+++ acoustid--1.0.sql
@@ -1,16 +1,16 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION acoustid" to load this file. \quit
 
-CREATE OR REPLACE FUNCTION acoustid_compare2(int4[], int4[], int DEFAULT 0) RETURNS float4
+CREATE OR REPLACE FUNCTION acoustid_compare2(int8[], int8[], int DEFAULT 0) RETURNS float4
     AS 'MODULE_PATHNAME'
     LANGUAGE C VOLATILE STRICT -- marked as VOLATILE to avoid multiple calls, even though the results are immutable
     COST 1000;
 
-CREATE OR REPLACE FUNCTION acoustid_compare3(int4[], int4[], int DEFAULT -1) RETURNS float4
+CREATE OR REPLACE FUNCTION acoustid_compare3(int8[], int8[], int DEFAULT -1) RETURNS float4
     AS 'MODULE_PATHNAME'
     LANGUAGE C VOLATILE STRICT -- marked as VOLATILE to avoid multiple calls, even though the results are immutable
     COST 1000;
 
-CREATE OR REPLACE FUNCTION acoustid_extract_query(int4[]) RETURNS int4[]
+CREATE OR REPLACE FUNCTION acoustid_extract_query(int8[]) RETURNS int8[]
     AS 'MODULE_PATHNAME'
     LANGUAGE C IMMUTABLE STRICT;
--- acoustid--unpackaged--1.0.sql
+++ acoustid--unpackaged--1.0.sql
@@ -1,6 +1,6 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION intarray FROM unpackaged" to load this file. \quit
 
-ALTER EXTENSION acoustid ADD function acoustid_compare2(int4[], int4[], int);
-ALTER EXTENSION acoustid ADD function acoustid_compare3(int4[], int4[], int);
-ALTER EXTENSION acoustid ADD function acoustid_extract_query(int4[]);
+ALTER EXTENSION acoustid ADD function acoustid_compare2(int8[], int8[], int);
+ALTER EXTENSION acoustid ADD function acoustid_compare3(int8[], int8[], int);
+ALTER EXTENSION acoustid ADD function acoustid_extract_query(int8[]);
